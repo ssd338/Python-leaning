8926494a3591d2dc6dc6bbbe81e529c104f7d74c
ssd338 <ssd2052@gmail.com>
2020-12-09 14:59:29 +0000

    tuple

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-a_String  = "like this"
-a_number = 3
-a_float = 3.12
-a_boolean = False #대문자로 시작
-a_none = None     
-''' None은 파이썬에서만 있는 규칙 null과 비슷한 의미 '''
-print(type(a_none))
+
+#===============================================================
+#tuple - 변경이 불가능한 열거 타입
+
+days=("Mon","Thu","Wed","Thur","Fri")
+print(type(days))
